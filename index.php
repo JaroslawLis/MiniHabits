3edfdf0fb97a0cee0fb940fd8d866d43b1cf0517
JaroslawLis <niejarek@poczta.onet.pl>
2019-11-17 21:58:20 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php require_once('private/initialize.php'); ?>
+
+
+
+<div >
+<h1>Mini Habits</h1>
+<form action="add_habit.php">
+<input type="text" name="habit" id="" placeholder="Nawyk">
+<input type="date" name="beggining_date" id="">
+<input type="submit" value="Dodaj nawyk">
+</form>
+
+</div>
+
+
+
+
---
+++ private/initialize.php
@@ -0,0 +1,29 @@
+<?php
+
+  ob_start(); // turn on output buffering
+
+
+  //require_once('functions.php');
+  //require_once('status_error_functions.php');
+  require_once('db_credentials.php');
+  require_once('database_functions.php');
+  //require_once('validation_functions.php');
+
+  foreach(glob('classes/*.class.php') as $file) {
+    require_once($file);
+  }
+
+  // Autoload class definitions
+  function my_autoload($class) {
+    if(preg_match('/\A\w+\Z/', $class)) {
+      include('classes/' . $class . '.class.php');
+    }
+  }
+  spl_autoload_register('my_autoload');
+
+  $database = db_connect();
+  Habit::set_database($database);
+  $res = Habit::find_all();
+ // var_dump($res);
+
+?>
